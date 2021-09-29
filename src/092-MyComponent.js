2324015407e1752c7e54052118758c6f8df5ba1a
yangux <yeonie05@gmail.com>
2021-09-29 17:09:21 +0900

    change format

--- src/092-MyComponent.js
+++ src/092-MyComponent.js
@@ -1,24 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({name, children, favoriteNumber}) => { // 비구조화 할당 문법 파라미터에 바로 쓰기
-    // const {name, children} = props; // 비구조화 할당 문법
-    return (
-        <div>
-            props.name: {name}, <br />
-            props.children: {children}, <br />
-            {favoriteNumber}의 PropTypes를 지정합니다.
-        </div>
-    )
-}
+const MyComponent = ({ name, children, favoriteNumber }) => {
+  // 비구조화 할당 문법 파라미터에 바로 쓰기
+  // const {name, children} = props; // 비구조화 할당 문법
+  return (
+    <div>
+      props.name: {name}, <br />
+      props.children: {children}, <br />
+      {favoriteNumber}의 PropTypes를 지정합니다.
+    </div>
+  );
+};
 
 MyComponent.defaultProps = {
-    name: '기본이름'
-}
+  name: '기본이름',
+};
 
 MyComponent.propTypes = {
-    name: PropTypes.string,
-    favoriteNumber: PropTypes.number.isRequired
-}
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
+};
 
 export default MyComponent;
--- src/102-MyComponent.js
+++ src/102-MyComponent.js
@@ -1,24 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
-    static defaultProps = {
-        name: '기본이름'
-    };
-    static propTypes = {
-        name: PropTypes.string,
-        favoriteNumber: PropTypes.number.isRequired
-    };
-    render() {
-        const {name, children, favoriteNumber} = this.props;
-        return (
-            <div>
-                props.name: {name}, <br />
-                props.children: {children}, <br />
-                {favoriteNumber}의 PropTypes를 지정합니다.
-            </div>
-        )
-    }
+  static defaultProps = {
+    name: '기본이름',
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
+  render() {
+    const { name, children, favoriteNumber } = this.props;
+    return (
+      <div>
+        props.name: {name}, <br />
+        props.children: {children}, <br />
+        {favoriteNumber}의 PropTypes를 지정합니다.
+      </div>
+    );
+  }
 }
 
 export default MyComponent;
