c9423406475580e3cad8b4ed479652b8e74ae0f1
yangux <yeonie05@gmail.com>
2021-10-01 16:02:00 +0900

    input 여러 개 다루기 - 함수형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import EventPractice from './124-EventPractice';
+import EventPractice from './138-EventPractice';
 
 const App = () => {
   return <EventPractice></EventPractice>;
