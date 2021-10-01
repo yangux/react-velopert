8a7cb858dd31b6e1cf3951c2ea0edd3ef2aca367
yangux <yeonie05@gmail.com>
2021-10-01 17:58:35 +0900

    컴포넌트에 ref 달기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import ValidationSample from './145-ValidationSample';
+import React, { Component } from 'react';
+import ScrollBox from './152-ScrollBox';
 
-const App = () => {
-  return <ValidationSample></ValidationSample>;
-};
+/*
+  ** 컴포넌트에 ref 달기 **
+  컴포넌트 외부에서 내부에 있는 메서드 및 멤버 변수에 접근할 수 있다
+  (ex: myComponent.handleClick, myComponent.input 등)
+*/
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
