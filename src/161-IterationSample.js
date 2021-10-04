673fa8fcd99fadb8565ef0bff5619673c7f93cc7
yangux <yeonie05@gmail.com>
2021-10-04 18:37:07 +0900

    컴포넌트 반복 + 데이터 추가 기능 구현하기 (concat)

--- src/161-IterationSample.js
+++ src/161-IterationSample.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  // 반복 렌더링 시 데이터가 가진 고윳값을 key 값으로 설정해야 함 (고윳값이 없을 때 index로 대체)
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      // concat으로 새로운 배열을 만들어 불변성 유지 (vs. push)
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText('');
+  };
+
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>;
+    </>
+  );
 };
 
 export default IterationSample;
